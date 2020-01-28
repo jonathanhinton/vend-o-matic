99a7431176f8c8336e6fc3315f010fc394f3755b
Jonathan Hinton <jonathanhinton@Jonathans-MacBook-Pro.local>
2020-01-27 19:38:22 -0600

    setup JSON models for interaction

--- index.js
+++ index.js
@@ -3,8 +3,36 @@ const express = require('express');
 
 const app = express();
 
+//Set up data for Inventory
+let inventory = [
+    {
+        id:"coke",
+        name:"Coca-Cola",
+        quantity:5
+    },
+    {
+        id:"diet_coke",
+        name:"Diet Coca-Cola",
+        quantity:5
+    },
+    {
+        id:"sprite",
+        name:"Sprite",
+        quantity:5
+    }
+];
+
+//Set up data for Vending Machine
+let vending_machine = {
+    coin_slot_height:"24.28mm",
+    coin_slot_width:"1.77mm",
+    session:false,
+    coin_cache:0,
+    inventory: inventory
+};
+
 app.get('/', (req, res) => {
-    res.send('Server Serving');
+    res.sendStatus(200);
 });
 
 const server = http.createServer(app);
